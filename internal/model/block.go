902874726627bf8f239b6d82c0e592cdb86e7c86
nnaakkaaii <ultraganbayu10@gmail.com>
2022-10-28 01:32:02 +0900

    add pow

--- internal/model/block.go
+++ internal/model/block.go
@@ -1,31 +1,16 @@
 package model
 
 import (
-	"bytes"
-	"crypto/sha256"
-	"strconv"
 	"time"
 )
 
 // Block contains its headers and some transactions.
-// Timestamp means the timestamp when a block generated.
-// Data means the actual data that includes some information.
-// PrevBlockHash refers to the hash of a previous block.
-// Hash stores the root hash of this block.
 type Block struct {
 	Timestamp     int64
 	Data          []byte
 	PrevBlockHash []byte
 	Hash          []byte
-}
-
-// SetHash concatenate block fields and calculate SHA-256 hash.
-func (b *Block) SetHash() {
-	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
-	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
-	hash := sha256.Sum256(headers)
-
-	b.Hash = hash[:]
+	Nonce         int
 }
 
 // NewBlock creates one Block struct
@@ -35,7 +20,13 @@ func NewBlock(data string, prevBlockHash []byte) *Block {
 		[]byte(data),
 		prevBlockHash,
 		[]byte{},
+		0,
 	}
-	block.SetHash()
+	pow := NewProofOfWork(block)
+	nonce, hash := pow.Run()
+
+	block.Hash = hash[:]
+	block.Nonce = nonce
+
 	return block
 }
---
+++ pkg/utils/cast_type.go
@@ -0,0 +1,17 @@
+package utils
+
+import (
+	"bytes"
+	"encoding/binary"
+	"log"
+)
+
+func IntToHex(num int64) []byte {
+	buff := new(bytes.Buffer)
+
+	if err := binary.Write(buff, binary.BigEndian, num); err != nil {
+		log.Panicln(err)
+	}
+
+	return buff.Bytes()
+}
