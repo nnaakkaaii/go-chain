3c97ae5a7935c75356a0579cfb9331d6eb12cedf
nnaakkaaii <ultraganbayu10@gmail.com>
2022-10-28 16:33:22 +0900

    divide commands

--- cmd/go-chain/commands/addblock.go
+++ cmd/go-chain/commands/addblock.go
@@ -2,30 +2,9 @@ package commands
 
 import (
 	"fmt"
-	"github.com/nnaakkaaii/go-chain/internal/model"
-	"strconv"
 )
 
 func (cli *CLI) addBlock(data string) {
 	cli.bc.AddBlock(data)
 	fmt.Println("Success!")
-}
-
-func (cli *CLI) printChain() {
-	bci := cli.bc.Iterate()
-
-	for {
-		block := bci.Next()
-
-		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
-		fmt.Printf("Data: %s\n", block.Data)
-		fmt.Printf("Hash: %x\n", block.Hash)
-		pow := model.NewProofOfWork(block)
-		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
-		fmt.Println()
-
-		if len(block.PrevBlockHash) == 0 {
-			break
-		}
-	}
 }
